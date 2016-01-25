0ee28020313c4561fbaadec76559d15732490008
Alain Pichonnat <alain.pichonnat@cpnv.ch>
2016-01-25 13:11:26 +0100

    update .gitignore and finih NewView.php

--- src/view/viewNew.php
+++ src/view/viewNew.php
@@ -1,5 +1,6 @@
 <form class="form" action="" method="get">
     <table class='table table-bordered table-new'>
+        <tr><td><a href="index.php">Retour</a></td></tr>
         <?php
             $nbchamp = 0;
             $value = $dt->nameChampbyTable($config['db']['table']);
